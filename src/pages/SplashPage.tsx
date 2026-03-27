da2e0ac927a66ea27fe157101bea5b4334d335f4
onedoonamoo-boop <onedoonamoo@gmail.com>
2026-03-27 03:20:30 +0000

    refactor: Update SplashPage

--- src/pages/SplashPage.tsx
+++ src/pages/SplashPage.tsx
@@ -1,6 +1,17 @@
 import SyncleMark from '@/components/SyncleMark'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function SplashPage() {
+  const navigate = useNavigate()
+  useEffect(() => {
+  const timer = window.setTimeout(() => {
+    navigate('/login', { replace: true })
+  }, 1000)
+
+  return () => window.clearTimeout(timer)
+}, [navigate])
   return (
     <div
       className="min-h-screen flex items-center justify-center p-8"
